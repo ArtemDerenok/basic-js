ba5381b2ac36247dadb1913f4c322c83ee6d4a57
Artem Derenok <derenokartem95@gmail.com>
2021-02-23 23:39:38 +0300

    update hanoi-tower.js

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -1,9 +1,19 @@
+const { match } = require("sinon");
 const CustomError = require("../extensions/custom-error");
 
-const MODERN_ACTIVITY= 15; 
-const HALF_LIFE_PERIOD= 5730;
+const MODERN_ACTIVITY = 15;
+const HALF_LIFE_PERIOD = 5730;
 
-module.exports = function dateSample(/* sampleActivity */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function dateSample(sampleActivity) {
+  if (
+    Number(sampleActivity) !== sampleActivity ||
+    Number(sampleActivity) < 0 ||
+    Number(sampleActivity) > 15 ||
+    typeof sampleActivity != "string"
+  ) {
+    return false;
+  } else {
+    let result = Math.log(15 / sampleActivity) / (0.693 / 5730);
+    return Math.ceil(result);
+  }
 };
